85de100f96f24068e6d29bf788525a456c1d8669
Jiří Polášek <me@jiripolasek.com>
2026-07-13 14:50:13 +0200

    Improve subscription to ticking ticker tick tock

--- src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.TimeDate/NowDockBand.cs
+++ src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.TimeDate/NowDockBand.cs
@@ -56,8 +56,7 @@ internal sealed partial class NowDockBand : ListItem, IDisposable
 
         UpdateText();
 
-        _clockUpdateService.Tick += ClockUpdateService_Tick;
-        _clockUpdateService.SetRequiresSecondUpdates(this, _timeWithSeconds);
+        _clockUpdateService.Subscribe(this, ClockUpdateService_Tick, _timeWithSeconds);
     }
 
     internal NowDockBand(IDockClockSettings settings, ICommand allClocksPage, Func<DateTime>? clock = null, ClockUpdateService? clockUpdateService = null)
@@ -134,8 +133,7 @@ internal sealed partial class NowDockBand : ListItem, IDisposable
 
     public void Dispose()
     {
-        _clockUpdateService.Tick -= ClockUpdateService_Tick;
-        _clockUpdateService.SetRequiresSecondUpdates(this, false);
+        _clockUpdateService.Unsubscribe(this);
         if (_ownsClockUpdateService)
         {
             _clockUpdateService.Dispose();
